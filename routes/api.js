2b8b946ff98b85eec50492ad2001c62dfb4a0f88
Qiwei Lin <Qiwei.l.ql@gmail.com>
2019-09-16 10:19:58 -0400

    quick break

--- routes/api.js
+++ routes/api.js
@@ -2,25 +2,36 @@ const express = require('express');
 const router  = express.Router();
 const axios = require('axios');
 
+/* GET home page */
+router.get('/', (req, res, next) => {
+  axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1000`)
+  .then(allPokes => {
+    // console.log(allPokes.data)
+    res.render('apiViews/apiHome', {allPokes : allPokes.data.results})
+    }).catch(err => next(err))
+});
 
 router.get('/poke/:pokeId', (req, res, next) => {
   //                |
   //                ------------------------------------------
   //                                                          |
-  axios.get(`https://pokeapi.co/api/v2/pokemon/${req.params.pokeId}`)
+  const pokeId = Number(req.params.pokeId) + 1;
+  console.log(pokeId);
+  
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
   .then(responseFromAPI => {
     console.log("><>><<<><><><><><> ", responseFromAPI.data);
 
     data = {
       pokes: responseFromAPI.data,
-      loadedInfo: true
+      isSaur: true
     };
 
     if(!responseFromAPI.data.name.includes('saur')) {
-      data.loadedInfo = false;
+      data.isSaur = false;
     }
 
-    res.render('apiViews/apiHome', data);
+    res.render('apiViews/apiDetails', data);
   }).catch(err => next(err));
 });
 
